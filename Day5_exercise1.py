bad25da47858f29be9e3c914749c5c7ce6a5bf68
sharifzodah <sharifzodah@gmail.com>
2023-03-14 16:58:19 -0400

    palindrome function in python

--- Day5_exercise1.py
+++ Day5_exercise1.py
@@ -5,6 +5,19 @@ print('===============================================')
 
 ips = ['100.122.133.105', '100.122.133.111']
 for i, j in enumerate(ips):
-    print(f'{i+1}.IP: {j}')
+    print(f'{i + 1}.IP: {j}')
 selection = input("Enter the number of the IP you want: ")
-print(ips[int(selection)-1])
+print(ips[int(selection) - 1])
+
+
+def isPalindrome(text):
+    r = len(text)
+    for k in range(0, r):
+        if text[k] != text[r - k - 1]:
+            return False
+    return True
+
+
+a = 'javv55aj'
+palindrome = isPalindrome(a)
+print(palindrome)
